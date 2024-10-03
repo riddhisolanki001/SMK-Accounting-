acd9b255d62cdc732da5c107585a31b2a4fd0ba3
Meet-BJarsaniya <meet@sanskartechnolab.com>
2024-10-03 17:17:14 +0530

    page expense breakup GST completed

--- smk_accounting/smk_petrochemicals_accounting/page/expences_breakup_gst/expences_breakup_gst.js
+++ smk_accounting/smk_petrochemicals_accounting/page/expences_breakup_gst/expences_breakup_gst.js
@@ -1,24 +1,67 @@
 frappe.pages['expences-breakup-gst'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Expences Breakup GST',
-		single_column: true
-	});
-
-    // Fetch the data via a server call
-    frappe.call({
-        method: "smk_accounting.smk_petrochemicals_accounting.page.expences_breakup_gst.expences_breakup_gst.make_data",
-        args: {
-            // date_after_1_day,
-            // date_after_10_days
-			// { date_after_1_day, date_after_10_days, data }
-        },
-        callback: function (response) {
-            const data = response.message
-			console.log(data)
-			const final_data = data[2]
-			console.log(final_data)
-            $(frappe.render_template("expences_breakup_gst", {final_data} )).appendTo(page.body);
-        }
+    var page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: 'Expences Breakup GST',
+        single_column: true
     });
+
+    // Define the fields for the filters with default values
+    let fields = [
+        {
+            label: 'From Date',
+            fieldtype: 'Date',
+            fieldname: 'from',
+            default: frappe.datetime.add_months(frappe.datetime.nowdate(), -1) // Default to 1 month ago
+        },
+        {
+            label: 'To Date',
+            fieldtype: 'Date',
+            fieldname: 'to',
+            default: frappe.datetime.nowdate() // Default to current date
+        },        
+    ];
+
+    let filter_values = {};
+
+    // Create a separate container for the data
+    let $data_container = $('<div id="data-container"></div>').appendTo(page.body);
+
+    // Create filter fields and set the default values
+    fields.forEach(field => {
+        let filterField = page.add_field(field);
+        // Set the initial value to the default specified in the field definition
+        filterField.set_input(field.default);
+        filter_values[field.fieldname] = field.default;
+
+        // Listen for changes in the filter values
+        filterField.$input.on('change', function() {
+            filter_values[field.fieldname] = filterField.get_value();
+            // Check if both filters have values and fetch the data
+            if (filter_values['from'] && filter_values['to']) {
+                fetch_and_render_data(filter_values['from'], filter_values['to']);
+            }
+        });
+    });
+
+    // Function to fetch and render data based on the filters
+    function fetch_and_render_data(from_date, to_date) {
+        frappe.call({
+            method: "smk_accounting.smk_petrochemicals_accounting.page.expences_breakup_gst.expences_breakup_gst.make_data",
+            args: {
+                from_date: from_date || '',
+                to_date: to_date || '',
+            },
+            callback: function(response) {
+                const data = response.message;
+                console.log(data);
+
+                // Clear only the data container and render new content
+                $data_container.empty();
+                $(frappe.render_template("expences_breakup_gst", { data })).appendTo($data_container);
+            }
+        });
+    }
+
+    // Initial call with default values
+    fetch_and_render_data(filter_values['from'], filter_values['to']);
 }
--- smk_accounting/smk_petrochemicals_accounting/page/expences_breakup_gst/expences_breakup_gst.py
+++ smk_accounting/smk_petrochemicals_accounting/page/expences_breakup_gst/expences_breakup_gst.py
@@ -1,44 +1,14 @@
 import frappe
-# from frappe.utils import add_days
 
 @frappe.whitelist()
-def make_data():
-    pi_query = """
-        SELECT
-            PI.name,
-            PII.expense_account,
-            PII.base_amount,
-            PII.is_ineligible_for_itc
-        FROM
-            `tabPurchase Invoice` AS PI
-        JOIN
-            `tabPurchase Invoice Item` AS PII ON
-            PI.name = PII.parent
-        WHERE
-            PI.docstatus = 1
-    """
-    pi_data = frappe.db.sql(pi_query, as_dict=True)
+def make_data(from_date=None, to_date=None):
     pi_query = """
         SELECT
             PII.expense_account,
-            SUM(PII.base_amount) AS total_base_amount,
-            PII.is_ineligible_for_itc
-        FROM
-            `tabPurchase Invoice` AS PI
-        JOIN
-            `tabPurchase Invoice Item` AS PII ON
-            PI.name = PII.parent
-        WHERE
-            PI.docstatus = 1
-        GROUP BY
-            PII.expense_account, PII.is_ineligible_for_itc
-    """
-    pii_data = frappe.db.sql(pi_query, as_dict=True)
-    pi_query = """
-        SELECT
-            PII.expense_account,
-            SUM(CASE WHEN PII.is_ineligible_for_itc = 1 THEN PII.base_amount ELSE 0 END) AS ineligible_amount,
-            SUM(CASE WHEN PII.is_ineligible_for_itc = 0 THEN PII.base_amount ELSE 0 END) AS eligible_amount,
+            SUM(CASE WHEN PI.gst_category="Unregistered" THEN PII.base_amount ELSE 0 END) AS unreg_supp_amount,
+            SUM(CASE WHEN PI.gst_category="Registered Regular" AND PII.is_ineligible_for_itc = 1 THEN PII.base_amount ELSE 0 END) AS ineligible_amount,
+            SUM(CASE WHEN PI.gst_category="Registered Regular" AND PII.is_ineligible_for_itc = 0 THEN PII.base_amount ELSE 0 END) AS eligible_amount,
+            SUM(CASE WHEN PI.gst_category="Registered Regular" THEN PII.base_amount ELSE 0 END) AS reg_supp_amount,
             SUM(PII.base_amount) AS total_amount
         FROM
             `tabPurchase Invoice` AS PI
@@ -46,8 +16,9 @@ def make_data():
             `tabPurchase Invoice Item` AS PII ON PI.name = PII.parent
         WHERE
             PI.docstatus = 1
+            AND (PI.posting_date BETWEEN %s AND %s OR  %s = '' OR %s = '')
         GROUP BY
             PII.expense_account
     """
-    piii_data = frappe.db.sql(pi_query, as_dict=True)
-    return pi_data, pii_data, piii_data
+    pi_data = frappe.db.sql(pi_query, (from_date, to_date, from_date, to_date), as_dict=True)
+    return pi_data
